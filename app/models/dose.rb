042734a2fe07a9ba97804f628c190e1516f45a7a
Paula Sturmer <paulasturmer@gmail.com>
2020-05-22 17:40:36 -0300

    Criação dos modelos

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence:true
+  validates :cocktail_id, presence:true
+  validates :ingredient_id, presence:true
+  validates :cocktail, uniqueness: { scope: :ingredient }
+end
